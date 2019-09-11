2c486161b2d0153f16508251ca50f8942b62a6a5
Val.istar.Guo <val.istar.guo@gmail.com>
2019-09-11 23:35:15 +0800

    feat: add server error class

---
+++ template/src/class/server-error.ts
@@ -0,0 +1,16 @@
+import { CustomError } from 'ts-custom-error'
+
+export type ErrorMessage = Readonly<string>
+
+export class ServerError extends CustomError {
+  expose: boolean
+
+  status: number
+
+  constructor(status: number, message: ErrorMessage) {
+    super(message)
+    this.status = status
+    this.expose = true
+    this.name = 'ServerError'
+  }
+}
